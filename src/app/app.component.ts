50ba229e3bda15e40d32a147afbb5dbee07d877d
royuuu <roy989898@gmail.com>
2020-10-26 15:28:39 +0800

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
     displayMobileTopBar = false;
+    breakpointObserverD?: Subscription;
 
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
-        private statusBar: StatusBar
+        private statusBar: StatusBar,
+        private  breakpointObserver: BreakpointObserver
     ) {
         this.initializeApp();
     }
@@ -25,7 +29,21 @@ export class AppComponent {
         this.platform.ready().then(() => {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
-            this.displayMobileTopBar = true;
+
+
+            this.breakpointObserverD = this.breakpointObserver.observe([
+                '(min-width: 576px)'
+            ]).subscribe(result => {
+                this.displayMobileTopBar = !result.matches;
+            });
         });
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    ngOnDestroy(): void {
+        this.breakpointObserverD?.unsubscribe();
     }
 }
