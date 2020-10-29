a9dc3d06cfd7e7c97725914e44bbc9c85fa456db
royuuu <roy989898@gmail.com>
2020-10-29 12:12:43 +0800

    save

--- src/app/page/timeline/timeline.page.ts
+++ src/app/page/timeline/timeline.page.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HasTopBarPage} from '../HasTopbarPage';
+import {TopBarStoreService} from '../../akita/TopBarStateStore/TopBarStoreService';
 
 @Component({
-  selector: 'app-timeline',
-  templateUrl: './timeline.page.html',
-  styleUrls: ['./timeline.page.scss'],
+    selector: 'app-timeline',
+    templateUrl: './timeline.page.html',
+    styleUrls: ['./timeline.page.scss'],
 })
 export class TimelinePage extends HasTopBarPage implements OnInit {
 
-  constructor() {
-    super();
-  }
+    constructor(private topBarStoreService: TopBarStoreService) {
+        super();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    ionViewWillEnter() {
+        this.topBarStoreService.updateTopState(false, false, false, true, false);
+        // console.warn('HomePage ionViewWillEnter');
+    }
 }
