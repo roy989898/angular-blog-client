6c023bbdb225d18d2418713e67a9d278cd4c42f0
royuuu <roy989898@gmail.com>
2020-10-30 16:14:39 +0800

    done blog detail header

--- src/app/page/blog-detail/blog-detail.module.ts
+++ src/app/page/blog-detail/blog-detail.module.ts
@@ -9,6 +9,9 @@ import { BlogDetailPageRoutingModule } from './blog-detail-routing.module';
 import { BlogDetailPage } from './blog-detail.page';
 import {TopBarModule} from '../../component/top-bar/top-bar.module';
 import {MarkedModule} from '../../pipe/marked/marked.module';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @NgModule({
     imports: [
@@ -17,7 +20,10 @@ import {MarkedModule} from '../../pipe/marked/marked.module';
         IonicModule,
         BlogDetailPageRoutingModule,
         TopBarModule,
-        MarkedModule
+        MarkedModule,
+        MatCardModule,
+        MatIconModule,
+        MatBadgeModule
     ],
     declarations: [BlogDetailPage]
 })
